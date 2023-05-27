981d2b03be8415c2b43aef0f3e590b1cbc41e984
kiran-nambiar <kiran.nambiar2@tifin.com>
2023-05-28 01:49:25 +0530

    changed path

--- app/global/constants.ts
+++ app/global/constants.ts
@@ -2,7 +2,7 @@ export const nonVisibleNavRoutes = ["/login", "/contact-us"];
 
 export const navigation = [
   { name: "About Us", href: "/about-us", current: true },
-  { name: "Contact Us", href: "/contact", current: false },
+  { name: "Contact Us", href: "/contact-us", current: false },
   { name: "Our Vision", href: "/vision", current: false },
 ];
 
