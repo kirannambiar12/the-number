fa5260aa302ea01443e8f75fee97362b6d79f023
kiran-nambiar <kiran.nambiar2@tifin.com>
2023-06-08 12:22:43 +0530

    added image tag

--- app/global/seo/index.tsx
+++ app/global/seo/index.tsx
@@ -19,21 +19,25 @@ const SeoHead = (props: any) => {
   return (
     <Head>
       <link rel="canonical" href={`${url}${asPath}`} />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon-precomposed" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="description"
-        content="Lookup and rate mobile numbers to protect yourself from scams. Search, rate, and comment on phone numbers to identify potential scammers and get detailed information about them at TheNumber.in."
+        content="Lookup and rate mobile numbers to protect yourself from scams. Search, rate, and comment on phone numbers to identify potential scammers and get detailed information about them at The Number"
       />
       <meta
         name="keywords"
-        content="mobile number lookup, scammer lookup, phone number lookup, phone number rating, phone number comments, scam identification, phone number details, protect from scams"
+        content="mobile number lookup, scammer lookup, phone number lookup, phone number rating, phone number comments, scam identification, phone number details, protect from scams, whatsapp scam, facebook scam, call scam"
       />
-      <meta name="author" content="TheNumber.in" />
-      <title>TheNumber.in - Mobile Number Lookup and Scam Identification</title>
+      <meta name="author" content="Kiran Nambiar" />
+      <title>The Number - Mobile Number Lookup and Scam Identification</title>
       <meta
         property="og:title"
         key="og:title"
-        content="TheNumber.in - Mobile Number Lookup and Scam Identification"
+        content="The Number - Mobile Number Lookup and Scam Identification"
       />
       <meta
         property="og:description"
@@ -42,6 +46,7 @@ const SeoHead = (props: any) => {
       />
       <meta property="og:url" content="https://www.thenumber.in/" />
       <meta property="og:site_name" content="The Number" />
+      <meta property="og:image" content="/logo.png" />
       <meta property="og:type" content="website" />
     </Head>
   );
